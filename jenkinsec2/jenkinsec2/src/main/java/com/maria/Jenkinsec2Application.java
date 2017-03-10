703f530f722bea7b16ddddecf6c967eacb768ff2
mariarathinam <mariarathinamwas@gmail.com>
2017-03-09 21:23:14 -0500

    First Commit

---
+++ jenkinsec2/jenkinsec2/src/main/java/com/maria/Jenkinsec2Application.java
@@ -0,0 +1,12 @@
+package com.maria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jenkinsec2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jenkinsec2Application.class, args);
+	}
+}
